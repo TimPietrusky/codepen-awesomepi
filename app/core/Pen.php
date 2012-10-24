281132a3335d51e63073977250500bd31a75b048
Tim Pietrusky <timpietrusky@googlemail.com>
2012-10-25 00:09:13 +0200

    Removed debug output.

--- app/core/Pen.php
+++ app/core/Pen.php
@@ -63,10 +63,6 @@ class Pen {
                 }
 
                 $this->addUrls();
-
-                echo "<pre>";
-                print_r($this->output);
-                die("</pre>");
             }
         } else {
             // Error
